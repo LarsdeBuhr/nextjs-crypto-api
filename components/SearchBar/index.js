37b6489889dbae80c874d15a9b74427fec6e711a
Lars <LarsdeBuhr@users.noreply.github.com>
2022-06-30 15:29:04 +0200

    initial commit

---
+++ components/SearchBar/index.js
@@ -0,0 +1,11 @@
+import styles from "./Search.module.css";
+
+const SearchBar = ({...rest}) => {
+  return (
+   <div className={styles.coin_search}>
+      <input className={styles.coin_input} {...rest}></input>
+   </div>
+  )
+}
+
+export default SearchBar
